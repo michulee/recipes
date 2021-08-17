99a9c7d58c6180e714c89c060ca7c2e6f64538e3
Michelle Lee <michellechulee@gmail.com>
2021-08-16 21:08:38 -0700

    Fixed async undefined value, Updated <Advertisement> and <Blog>
    Switched from using for loop to map with correct syntax

--- client/src/components/Advertisement/Advertisement.js
+++ client/src/components/Advertisement/Advertisement.js
@@ -13,28 +13,19 @@ export default function Advertisement(props) {
   //   );
   // }
 
-  const List = (props) => {
+  const AdvertiseList = (props) => {
     const photos = props.src;
-    // const listOfCards = photos.map((photo) => {
-    //   return(
-    //     <div className="row" key={photo.id} >
-    //       <SmallCardSquare className="card" src={photo.urls.small} alt={photo.alt_description}/>
-    //       <div>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</div>
-    //     </div>
-    //   )
-    // });
 
-    const listOfCards = [];
-
-    for (let i = 0; i < props.items; i++) {
-      // 0 and even
-      if (i % 2 === 0) {
-        listOfCards.push(
-            <div className="row align-left" key={photos[i].id}>
+    return (
+      <>
+        {photos.map((photo, i) => {
+          // 0 and even
+          if (i % 2 === 0) {
+            return <div className="row align-left" key={photo.id}>
               <img
                 className="card"
-                src={photos[i].urls.small}
-                alt={photos[i].alt_description}
+                src={photo.urls.small}
+                alt={photo.alt_description}
               />
               <div className="adjust">
                 <h3>Delicious avocado toast</h3>
@@ -46,12 +37,10 @@ export default function Advertisement(props) {
                   disruptive innovation via workplace diversity and empowerment.
                 </p>
               </div>
-            </div>
-        );
-      } else {
-        //odd
-        listOfCards.push(
-            <div className="row align-right" key={photos[i].id}>
+            </div>;
+          } else {
+            //odd
+            return <div className="row align-right" key={photo.id}>
               <div className="adjust">
                 <h3>Delicious fish with sauce</h3>
                 <p>
@@ -64,21 +53,14 @@ export default function Advertisement(props) {
               </div>
               <img
                 className="card"
-                src={photos[i].urls.small}
-                alt={photos[i].alt_description}
+                src={photo.urls.small}
+                alt={photo.alt_description}
               />
-            </div>
-        );
-      }
-    }
-
-    //this works with photos.map((photo) => <SmallCard/>)
-    // return(
-    //   <div>{listOfCards}</div>
-    // );
-
-    //this works with photos.map((photo) => return <SmallCard/>)
-    return listOfCards;
+            </div>;
+          }
+        })}
+      </>
+    );
   };
 
   //   https://codesandbox.io/s/beating-async-race-conditions-in-react-7759f?file=/src/DataDisplayer.js
@@ -96,14 +78,8 @@ export default function Advertisement(props) {
   if (advertisement) {
     return (
       <div className="advertisement">
-        {/* <h2 className="subheader">Editor's Choice</h2> */}
-
-        {/* <div className="grid">
-          <CardList src={photos}/>
-        </div> */}
-
         <div className="grid">
-          <List src={advertisement} items={props.items} />
+          <AdvertiseList src={advertisement} items={props.items} />
         </div>
       </div>
     );
--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -12,107 +12,102 @@ import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
 
 export default function Blog(props) {
   const [photos, setPhotos] = useState([]);
-  //
 
-  // const CardItem = (props) => {
-  //   // return <SmallCard src={props.url}/>
-  //   return(
-
-  //   );
-  // }
-
-  const CardList = (props) => {
-    const photos = props.src;
-    // console.log(photos)
-
-    // const listOfCards = photos.map((photo) => {
-    //   return(
-    //     <div className="row" key={photo.id} >
-    //       <SmallCardSquare className="card" src={photo.urls.small} alt={photo.alt_description}/>
-    //       <div>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</div>
-    //     </div>
-    //   )
-    // });
-
-    const listOfCards = [];
-
-    for (let i = 0; i < props.items; i++) {
-      // 0 and even
-      if (i % 2 === 0) {
-        listOfCards.push(
-          <div className="row " key={photos[i].id}>
-            <SmallCardSquare className="card"  src={photos[i].urls.small} alt={photos[i].alt_description}/>
-            <div className="adjust">
-              <h3>Delicious avocado toast</h3>
-              <p>
-                Leverage agile frameworks to provide a robust synopsis for
-                high level overviews. Iterative approaches to corporate
-                strategy foster collaborative thinking to further the overall
-                value proposition. Organically grow the holistic world view of
-                disruptive innovation via workplace diversity and empowerment.
-              </p>
-            </div>
-          </div>
-        );
-      } else {
-        listOfCards.push(
-          <div className="row align-right" key={photos[i].id}>
-            <div className="adjust">
-              <h3>Delicious fish with sauce</h3>
-              <p>
-                Leverage agile frameworks to provide a robust synopsis for
-                high level overviews. Iterative approaches to corporate
-                strategy foster collaborative thinking to further the overall
-                value proposition. Organically grow the holistic world view of
-                disruptive innovation via workplace diversity and empowerment.
-              </p>
-            </div>
-            <SmallCardSquare
-              className="card"
-              src={photos[i].urls.small}
-              alt={photos[i].alt_description}
-            />
-          </div>
-        );
-      }
-    }
-
-    //this works with photos.map((photo) => <SmallCard/>)
-    // return(
-    //   <div>{listOfCards}</div>
-    // );
-
-    //this works with photos.map((photo) => return <SmallCard/>)
-    return listOfCards;
-  };
-
-  //   https://codesandbox.io/s/beating-async-race-conditions-in-react-7759f?file=/src/DataDisplayer.js
+  // https://codesandbox.io/s/beating-async-race-conditions-in-react-7759f?file=/src/DataDisplayer.js
   // componentDidUpdate() fires with new props or setState or forceUpdate
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
       const data = await unsplash.collections.getPhotos({
         collectionId: "e6Ypx3GRJLw",
       });
-      setPhotos(data.response.results);
+      // setPhotos(data.response.results);
+
+      if (isMounted) {
+        setPhotos(data.response.results);
+      }
     };
     fetchData();
-  });
+    return () => {
+      isMounted = false;
+    };
 
-  if (photos) {
+    // console.log(photos)
+    // photos.map(item => console.log(item.urls.regular))
+  }, []);
+
+  const CardList = (props) => {
+    const photos = props.src;
     return (
-      <div className="blog">
-        <h2 className="subheader">Editor's Choice</h2>
+      <>
+        {photos.map((photo, i) => {
+          if (i % 2 === 0) {
+            return (
+              <div className="row " key={photo.id}>
+                <SmallCardSquare
+                  className="card"
+                  src={photo.urls.small}
+                  alt={photo.alt_description}
+                />
+                <div className="adjust">
+                  <h3>Delicious avocado toast</h3>
+                  <p>
+                    Leverage agile frameworks to provide a robust synopsis for
+                    high level overviews. Iterative approaches to corporate
+                    strategy foster collaborative thinking to further the
+                    overall value proposition. Organically grow the holistic
+                    world view of disruptive innovation via workplace diversity
+                    and empowerment.
+                  </p>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className="row align-right" key={photo.id}>
+                <div className="adjust">
+                  <h3>Delicious fish with sauce</h3>
+                  <p>
+                    Leverage agile frameworks to provide a robust synopsis for
+                    high level overviews. Iterative approaches to corporate
+                    strategy foster collaborative thinking to further the
+                    overall value proposition. Organically grow the holistic
+                    world view of disruptive innovation via workplace diversity
+                    and empowerment.
+                  </p>
+                </div>
+                <SmallCardSquare
+                  className="card"
+                  src={photo.urls.small}
+                  alt={photo.alt_description}
+                />
+              </div>
+            );
+          }
+        })}
+      </>
+    );
+  };
 
-        {/* <div className="grid">
+  // if (photos) {
+  return (
+    <div className="blog">
+      <div className="subheader">
+        <h2 className="title">Editor's Choice</h2>
+        <FontAwesomeIcon icon={faArrowAltCircleRight}/>
+      </div>
+
+      {/* <div className="grid">
           <CardList src={photos}/>
         </div> */}
 
-        <div className="grid">
-          <CardList src={photos} items={props.items} />
-        </div>
+      <div className="grid">
+        <CardList src={photos} items={props.items} />
       </div>
-    );
-  } else {
-    return null;
-  }
+    </div>
+  );
+  // }
+  // else {
+  //   return null;
+  // }
 }
