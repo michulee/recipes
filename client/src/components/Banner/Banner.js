d52a2120790b59784f1ad11caa9803c423bfa3ca
Michelle Lee <michellechulee@gmail.com>
2021-10-01 14:55:20 -0700

    modular CSS, useEffect mounting

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -4,27 +4,42 @@ import "./_Banner.scss";
 import unsplash from "../../api/unsplash";
 
 export default function Banner(props) {
-  const [state, setState] = useState({
-    banner: "",
-  });
+  // const [state, setState] = useState({
+  //   banner: "",
+  // });
+
+  const [state, setState] = useState('');
 
   //   https://codesandbox.io/s/beating-async-race-conditions-in-react-7759f?file=/src/DataDisplayer.js
   // componentDidUpdate() fires with new props or setState or forceUpdate
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
       const data = await unsplash.photos.get({ photoId: "nBtmglfY0HU" });
-      setState({ ...state, banner: data.response });
+      if(isMounted) {
+        // setState({ ...state, banner: data.response });
+        setState(data.response);
+      }
     };
     fetchData();
-  });
+    return () => {
+      isMounted = false;
+    }
+  }, []);
 
-  if (state.banner) {
-    return (
-      <div className="banner">
-        <img src={state.banner.urls.regular} alt={state.banner.description} />
-      </div>
-    );
-  } else {
-    return null;
-  }
+  // return state && (
+  //   <div className="banner">
+  //     {console.log(state)}
+  //     {/* <img src={state.banner.urls.regular} alt={state.banner.description} /> */}
+  //     <img src={state.urls.regular} alt={state.description} />
+  //   </div>
+  // );
+
+  const test = 0;
+  return (
+    <div className="banner">
+      {/* <img src={state.banner.urls.regular} alt={state.banner.description} /> */}
+      {state && <img src={state.urls.regular} alt={state.description} />}
+    </div>
+  );
 }
--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -25,6 +25,7 @@ export default function Blog(props) {
 
       if (isMounted) {
         setPhotos(data.response.results);
+        console.log(photos)
       }
     };
     fetchData();
